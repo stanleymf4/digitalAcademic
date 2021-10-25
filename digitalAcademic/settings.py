3466abaddffad48985d3ac64aa1b80c80b2337b0
ssmelof <ss.melo@uniandes.edu.co>
2021-10-25 09:45:59 -0500

    OK3

--- digitalAcademic/settings.py
+++ digitalAcademic/settings.py
@@ -92,23 +92,23 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'digitalacademic',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'marvaju',
-    #     'USER': 'digital_proccess',
-    #     'PASSWORD': 'Maq.81091081',
+    #     'NAME': 'digitalacademic',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
     #     'HOST': '127.0.0.1',
     #     'PORT': '5432',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'digital_proccess',
+        'USER': 'marvaju',
+        'PASSWORD': 'Maq.81091081',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 
 
